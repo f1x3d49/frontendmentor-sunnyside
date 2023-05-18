880da400754e655deec13e2816e4aa8d3aa0f74c
f1x3d49 <davidnicoara28@gmail.com>
2023-05-18 15:33:31 +0300

    did the first part

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import morange from "../images/mobile/image-header.jpg";
 import dorange from "../images/desktop/image-header.jpg";
 import Navbar from "./Navbar";
+import { ReactComponent as ADown } from "../images/icon-arrow-down.svg";
 
 const Hero = () => {
   return (
@@ -12,6 +13,12 @@ const Hero = () => {
         <source srcSet={morange} />
         <img className="-mt-24 object-center md:-mt-28" src={morange} alt="" />
       </picture>
+      <h1 className="absolute w-screen text-center top-[20%] uppercase   tracking-wider font-fraunces text-nwhite text-7xl">
+        We are creatives
+      </h1>
+      <div className="absolute top-[44%] left-[49%]">
+        <ADown />
+      </div>
     </section>
   );
 };
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,24 +1,50 @@
 import React from "react";
+import { Menu } from "@headlessui/react";
 import { ReactComponent as AgencyLogo } from "../images/logo.svg";
+import { ReactComponent as Hamburger } from "../images/icon-hamburger.svg";
 
 const Navbar = () => {
   return (
     <nav className="relative flex justify-between items-center z-10 py-3 px-8 ">
       <AgencyLogo />
-      <ul className="flex">
-        <li className="p-4 font-barlow font-semibold text-nwhite text-lg">
+      <ul className="hidden md:flex select-none">
+        <li className="p-4 font-barlow font-semibold text-nwhite text-lg hover:cursor-pointer">
           About
         </li>
-        <li className="p-4 font-barlow font-semibold text-nwhite text-lg">
+        <li className="p-4 font-barlow font-semibold text-nwhite text-lg hover:cursor-pointer">
           Services
         </li>
-        <li className="p-4 font-barlow font-semibold text-nwhite text-lg">
+        <li className="p-4 font-barlow font-semibold text-nwhite text-lg hover:cursor-pointer">
           Projects
         </li>
         <li className="p-4 text-xl font-fraunces text-vddblue bg-nwhite rounded-full hover:cursor-pointer active:bg-gblue active:text-nwhite">
           Contact
         </li>
       </ul>
+      <Menu as="div" className="md:hidden relative inline-flex text-center">
+        <Menu.Button>
+          <Hamburger />
+        </Menu.Button>
+        <Menu.Items className="absolute flex flex-col items-center p-8 gap-8 z-10 right-0 w-96 top-20 bg-nwhite shadow-sm ring-1 ring-nwhite text-lg font-barlow text-dgblue arrow">
+          <Menu.Item>
+            <a href="/">About</a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="/">Services</a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="/">Projects</a>
+          </Menu.Item>
+          <Menu.Item>
+            <a
+              href="/"
+              className="bg-pyellow w-1/2 py-3 rounded-full font-fraunces text-vddblue font-semibold text-xl"
+            >
+              Contact
+            </a>
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
     </nav>
   );
 };
